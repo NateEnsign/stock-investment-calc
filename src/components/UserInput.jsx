ca77117500068460bab2718b2a5ba8f2932238ac
Nate Ensign <dixienate@gmail.com>
2024-12-02 15:00:27 -0700

    userInput component created

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,56 @@
+
+
+const UserInput = ({onChange, userInput}) => {
+  return (
+    <section id="user-input">
+      <div clasName="input-group">
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestement}
+            onChange={(event) =>
+              onChange("initialInvestement", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              onChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div clasName="input-group">
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              onChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => onChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default UserInput;
