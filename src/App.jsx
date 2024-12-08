fbd6f08c461e1a6b99174113143d4f5f440f74fb
Nate Ensign <dixienate@gmail.com>
2024-12-08 13:48:24 -0700

    problem with output data not rendering correctly has been fixed. Small typo was causing the issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Results from "./components/Results";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestement: 10000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
